ca98e46db92f25776f0058d05a8ed439071030b7
Maciel Ramos de jesus <macielramosdejesus@gmail.com>
2022-08-12 22:21:40 -0300

    Upload  de  todos  os  arquivos do  projeto V1.0
    Upload de  arquivos, infelizmente  por  conta  do pouco  tempo que  tive não  pude  adicionar os  arrays nas  telas, peço seja  compreensivo nesse  quesito, pois não  tive muita familiaridade com a  linguagem de  início  estou a  disposição pra  qualquer dúvida, sob RA 15485395

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Marmitas  Dona  Rita</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.png" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Culinária Cadeseira</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Início</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Sobre</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cozinha</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">Pratos Típcos</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Marmitex</a></li>
+                                <li><a class="dropdown-item" href="#!">Pratos  da  casa</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                   
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                <img src="assets/logo.png" alt="logomarca" height="100px" width="100px"/><h1 class="display-4 fw-bolder"> Marmitas  Dona  Rita</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Todo sabor e toque caseiro em um só prato</p>
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://www.qgjeitinhocaseiro.com/wp-content/uploads/2019/11/Blog-1.png" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Vegetais  frescos</h5>
+                                    <!-- Product price-->
+                                    R$ 30,00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">  Ver mais... </a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">marmita + salada</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://saude.abril.com.br/wp-content/uploads/2021/05/alimentac%CC%A7a%CC%83o-marmita-Dulla.png?w=680&resize=1200,800" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Tempero da Rita</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">R$ 20,00</span>
+                                    R$18,00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">Ver mais... </a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">mais pedida</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://img.freepik.com/fotos-premium/marmita-ou-marmitex-almoco-entregue-em-domicilio-brasileiro-com-farofa-de-linguica-de-arroz-e-feijao-de-salada-copia-espaco-fundo-amarelo_72932-3447.jpg?w=2000" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Tradicional</h5>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">R$30,00</span>
+                                    R$25,00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">Ver mais... </a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://naturalchef.com.br/blog/wp-content/uploads/2021/09/Como-Montar-uma-Marmita-PerfeitaPrancheta-1-min.jpg" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Tradicional tam. médio</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    R$12,00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">Ver Mais...</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">pague 1 leve 2 </div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://marmitas.saudegourmetfit.com.br/imagens/marmita-fitness-congelada-preco-sob-encomenda.png" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Fitness</h5>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">$50.00</span>
+                                    $35.00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">Ver mais...</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://marmitas.saudegourmetfit.com.br/imagens/valor-marmitas-fitness.png" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Vegano</h5>
+                                    <!-- Product price-->
+                                   R$ 23,00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">Ver mais...</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Leve 3</div>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://www.acasaencantada.com.br/wp-content/uploads/2019/09/marmita-fitness.png" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Combo fit família</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    <span class="text-muted text-decoration-line-through">R$80,00</span>
+                                    R$55,00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">Ver mais...</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="https://img.freepik.com/fotos-premium/marmita-ou-marmitex-almoco-entregue-em-domicilio-brasileiro-com-farofa-de-coxa-de-frango-arroz-e-feijao-de-salada-copia-espaco-fundo-amarelo_72932-3220.jpg?w=2000" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Tradicional completa</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    R$30,00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="marmitas.php">Ver mais...</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">  
+        <div class="container">
+         <p class="lead fw-normal text-white-50 mb-0">   Quer  Saber  mais <p>
+ <ol class="lead fw-normal text-white-50 mb-0">
+<a href="#">Início</a><br>
+<a href="#">Blog</a><br>
+<a href="#">Conheça Dona Rita</a>
+</ol><br>
+<ol class="lead fw-normal text-white-50 mb-0"  >
+<a href="#">Politica de  privacidade</a><br>
+<a href="#">Aviso Legal</a><br>
+<a href="#">Termos de uso</a><br>
+<a href="#">Contato</a><br>
+</ol>
+
+
+</div>
+
+</ul>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy;Aluno Maciel Ramos de Jesus - Sob RA 15485395 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ marmitas.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Marmitas  Dona  Rita</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.png" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Culinária Cadeseira</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Início</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Sobre</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Cozinha</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">Pratos Típcos</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Marmitex</a></li>
+                                <li><a class="dropdown-item" href="#!">Pratos  da  casa</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                   
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                <img  src="https://www.qgjeitinhocaseiro.com/wp-content/uploads/2019/11/Blog-1.png" alt="marmita" wight="425px" heght="425px"/><h1 class="display-4 fw-bolder"> Vegetais  frescos</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">
+                        <ol>
+                            <ul>Brócolis</ul>
+                            <ul>Cenoura</ul>
+                            <ul>Pepino</ul>
+                            <ul>Macarrão Vegetal</ul>
+                            <ul>Frango</ul>                          
+                        </ol>
+                        <img  src="https://saude.abril.com.br/wp-content/uploads/2021/05/alimentac%CC%A7a%CC%83o-marmita-Dulla.png?w=425&resize=600,425" alt="marmita" wight="425px" heght="425px"/><h1 class="display-4 fw-bolder"> Tempero da Rita</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">
+                        <ol>
+                            <ul>Brócolis</ul>
+                            <ul>Cenoura</ul>
+                            <ul>Pepino</ul>
+                            <ul>Macarrão Vegetal</ul>
+                            <ul>Frango</ul>                          
+                        </ol>
+                       
+                    <p class="lead fw-normal text-white-50 mb-0">
+                    <img  src="https://img.freepik.com/fotos-premium/marmita-ou-marmitex-almoco-entregue-em-domicilio-brasileiro-com-farofa-de-linguica-de-arroz-e-feijao-de-salada-copia-espaco-fundo-amarelo_72932-3447.jpg?w=425" alt="marmita" wight="100px" heght="100px"/><h1 class="display-4 fw-bolder"> Tradicional</h1>
+                        <ol>
+                            <ul>Feijão</ul>
+                            <ul>Linguiça</ul>
+                            <ul>Ovos</ul>
+                            <ul>Arroz branco</ul>
+                            <ul>ervilha</ul> 
+                            <ul>Salada mista</ul>                         
+                        </ol>
+                </div>
+            </div>
+        </header>
+      
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">  
+        <div class="container">
+         <p class="lead fw-normal text-white-50 mb-0">   Quer  Saber  mais <p>
+ <ol class="lead fw-normal text-white-50 mb-0">
+<a href="index.php">Início</a><br>
+<a href="#">Blog</a><br>
+<a href="#">Conheça Dona Rita</a>
+</ol><br>
+<ol class="lead fw-normal text-white-50 mb-0">
+<a href="#">Politica de  privacidade</a><br>
+<a href="#">Aviso Legal</a><br>
+<a href="#">Termos de uso</a><br>
+<a href="#">Contato</a><br>
+</ol>
+
+
+</div>
+
+</ul>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy;Aluno Maciel Ramos de Jesus - Sob RA 15485395 2022</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
